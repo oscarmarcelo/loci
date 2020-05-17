41384e5274f0a805593f5818683af04ecad70a17
Oscar Marcelo <oscarmarcelo@users.noreply.github.com>
2020-05-17 13:53:43 +0100

    [WIP] Add generator for TLD
    - We still need to make it locale-aware. Faker isn't specific enough.

--- src/renderer/src/scripts/data/internet/tld.js
+++ src/renderer/src/scripts/data/internet/tld.js
@@ -1,19 +1,27 @@
+import faker from 'faker';
+
+import {language, textTransform} from '../utils';
+
+
+
 const config = {
   id: 'tld',
   name: 'Top-Level Domain',
   fields: [
-    {
-      type: 'languages'
-    },
-    {
-      type: 'checkbox',
-      id: 'common-tlds',
-      text: 'Only common TLDs',
-      description: 'Use only "com", "net", "org", and country TLDs related to the selected languages.'
-    },
-    {
-      type: 'separator'
-    },
+    // TODO: Faker locales isn't good enough.
+    //       Also consider option to only use country TLDs.
+    // {
+    //   type: 'languages'
+    // },
+    // {
+    //   type: 'checkbox',
+    //   id: 'common-tlds',
+    //   text: 'Only common TLDs',
+    //   description: 'Use only "com", "net", "org", and country TLDs related to the selected languages.'
+    // },
+    // {
+    //   type: 'separator'
+    // },
     {
       type: 'heading-2',
       text: 'Text Options'
@@ -27,17 +35,11 @@ const config = {
 
 
 function generator(options) {
-  // if (options.language) {
-  //   window.faker.locale = options.language;
-  // }
+  const _language = language(options.languages);
 
-  // let result = window.faker.name.firstName(options.gender);
+  faker.locale = _language;
 
-  // if (options.textTransform) {
-  //   // TODO:
-  // }
-
-  // return result;
+  return textTransform(faker.internet.domainSuffix(), options['text-transform'], _language);
 }
 
 
