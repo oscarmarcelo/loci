7fc7dddd638aa6f86750c12009a38e5a27256f60
Oscar Marcelo <oscarmarcelo@users.noreply.github.com>
2020-05-23 13:32:07 +0100

    Refactor constants to use variables

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,11 +1,16 @@
+const pluginId = 'com.oscarmarcelo.loci';
+const mainWindow = 'main';
+const tokenPopover = 'token-popover';
+const selectPopover = 'select-popover';
+
 export default {
   PLUGIN_NAME: 'Loci',
-  PLUGIN_ID: 'com.oscarmarcelo.loci',
-  MAIN_WINDOW_ID: 'loci.main',
-  MAIN_WINDOW_OBSERVERS: 'loci.main.observers',
-  TOKEN_POPOVER_WINDOW_ID: 'loci.token-popover',
-  DATA_SUGGESTIONS_WINDOW_ID: 'loci.select-popover.data-suggestions',
-  DATA_LIST_WINDOW_ID: 'loci.select-popover.data-list',
+  PLUGIN_ID: pluginId,
+  MAIN_WINDOW_ID: `${pluginId}.${mainWindow}`,
+  MAIN_WINDOW_OBSERVERS: `${pluginId}.${mainWindow}.observers`,
+  TOKEN_POPOVER_WINDOW_ID: `${pluginId}.${tokenPopover}`,
+  DATA_SUGGESTIONS_WINDOW_ID: `${pluginId}.${selectPopover}.data-suggestions`,
+  DATA_LIST_WINDOW_ID: `${pluginId}.${selectPopover}.data-list`,
   DATA_SCRIPTS_ID: 'scripts',
   DATA_SUPPLIER_ACTION: 'SupplyData'
 };
