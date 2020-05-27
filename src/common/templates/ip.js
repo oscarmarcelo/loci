0a32a57d52eb752b33a6baaa4366c50b7bc95c9c
Oscar Marcelo <oscarmarcelo@users.noreply.github.com>
2020-05-27 20:45:18 +0100

    [WIP] Add "IP" template
    Still needs to implement integration of templates

---
+++ src/common/templates/ip.js
@@ -0,0 +1,59 @@
+export default {
+  name: 'IP',
+  data: {
+    tokens: [
+      {
+        type: 'data',
+        config: {
+          data: {
+            group: 'random',
+            item: 'number'
+          },
+          'limit-max': 255
+        }
+      },
+      {
+        type: 'text',
+        text: '.'
+      },
+      {
+        type: 'data',
+        config: {
+          data: {
+            group: 'random',
+            item: 'number'
+          },
+          'limit-max': 255
+        }
+      },
+      {
+        type: 'text',
+        text: '.'
+      },
+      {
+        type: 'data',
+        config: {
+          data: {
+            group: 'random',
+            item: 'number'
+          },
+          'limit-max': 255
+        }
+      },
+      {
+        type: 'text',
+        text: '.'
+      },
+      {
+        type: 'data',
+        config: {
+          data: {
+            group: 'random',
+            item: 'number'
+          },
+          'limit-max': 255
+        }
+      }
+    ]
+  }
+};
