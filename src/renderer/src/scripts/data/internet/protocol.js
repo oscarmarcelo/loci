53fb63c8e3f2f2d7d3f2751f2de8a8c6b66e1f0e
Oscar Marcelo <oscarmarcelo@users.noreply.github.com>
2020-05-28 12:39:27 +0100

    Add `text-transform` field to `prototol` data

--- src/renderer/src/scripts/data/internet/protocol.js
+++ src/renderer/src/scripts/data/internet/protocol.js
@@ -1,17 +1,27 @@
 import faker from 'faker';
 
+import {textTransform} from '../utils';
+
 
 
 const config = {
   id: 'protocol',
   name: 'HTTP Protocol',
-  fields: []
+  fields: [
+    {
+      type: 'heading-2',
+      text: 'Text Options'
+    },
+    {
+      type: 'text-transform'
+    }
+  ]
 };
 
 
 
-function generator() {
-  return faker.internet.protocol();
+function generator(options) {
+  return textTransform(faker.internet.protocol(), options['text-transform'], 'en');
 }
 
 
