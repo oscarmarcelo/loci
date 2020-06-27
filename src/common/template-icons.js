0da8035065501b06d020c683d4f39e3484804027
Oscar Marcelo <oscarmarcelo@users.noreply.github.com>
2020-06-27 19:53:29 +0100

    Add the first batch of Template icons

---
+++ src/common/template-icons.js
@@ -0,0 +1,101 @@
+export default [
+  {
+    id: 'objects-tools',
+    name: 'Objects & Tools',
+    items: [
+      {
+        id: 'pencil',
+        icon: 'pencil',
+        name: 'Pencil'
+      },
+      {
+        id: 'pencil-and-outline',
+        icon: 'pencil-and-outline',
+        name: 'Pencil and Outline',
+        alternatives: [
+          'scribble',
+          'circle',
+          'drawing',
+          'sketch'
+        ]
+      },
+      {
+        id: 'trash',
+        icon: 'trash',
+        name: 'Trash',
+        alternatives: [
+          'Bin',
+          'Bucket',
+          'Container'
+        ]
+      }
+    ]
+  },
+  {
+    id: 'connectivity',
+    name: 'Connectivity',
+    items: [
+      {
+        id: 'network',
+        icon: 'network',
+        name: 'Network'
+      }
+    ]
+  },
+  {
+    id: 'human',
+    name: 'Human',
+    items: [
+      {
+        id: 'person',
+        icon: 'person',
+        name: 'Person',
+        alternatives: [
+          'User',
+          'Human',
+          'People',
+          'Man',
+          'Men',
+          'Woman',
+          'Women'
+        ]
+      },
+      {
+        id: 'ear',
+        icon: 'ear',
+        name: 'Ear'
+      },
+      {
+        id: 'thumbs-up',
+        icon: 'thumbs-up',
+        name: 'Thumbs Up',
+        alternatives: [
+          'Hands',
+          'Finger'
+        ]
+      }
+    ]
+  },
+  {
+    id: 'nature',
+    name: 'Nature',
+    items: [
+      {
+        id: 'flame',
+        icon: 'flame',
+        name: 'Flame',
+        alternatives: [
+          'Fire'
+        ]
+      },
+      {
+        id: 'bolt',
+        icon: 'bolt',
+        name: 'Bolt',
+        alternatives: [
+          'Lightning'
+        ]
+      }
+    ]
+  }
+];
