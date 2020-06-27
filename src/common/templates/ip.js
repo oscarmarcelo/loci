298f71441b856eec3acf47f7dab6cf34970ca886
Oscar Marcelo <oscarmarcelo@users.noreply.github.com>
2020-06-27 20:32:08 +0100

    Update "IP" Template to have an icon and token IDs

--- src/common/templates/ip.js
+++ src/common/templates/ip.js
@@ -1,8 +1,10 @@
 export default {
   name: 'IP',
+  icon: 'network',
   data: {
     tokens: [
       {
+        id: '77b454df-d51f-42e1-a253-7381c3ba2689',
         type: 'data',
         config: {
           data: {
@@ -13,10 +15,12 @@ export default {
         }
       },
       {
+        id: '3c34c8c7-2c26-45f8-ad47-649229b26265',
         type: 'text',
         text: '.'
       },
       {
+        id: '677199e9-ff2e-4d66-a87b-8d29e1e4d5a7',
         type: 'data',
         config: {
           data: {
@@ -27,10 +31,12 @@ export default {
         }
       },
       {
+        id: '3f097281-d8c8-41b8-b486-19f6a5047c79',
         type: 'text',
         text: '.'
       },
       {
+        id: '4401be40-758a-4157-b0a5-2dac0eb6a293',
         type: 'data',
         config: {
           data: {
@@ -41,10 +47,12 @@ export default {
         }
       },
       {
+        id: '42db55cc-a2ad-4c85-8fbe-1bc7ca31b2d1',
         type: 'text',
         text: '.'
       },
       {
+        id: 'b0069b71-3b55-4ff4-aa0c-d2b401d0e87d',
         type: 'data',
         config: {
           data: {
