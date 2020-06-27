15e7983ed389f1d35539618359250c6c8dee4132
Oscar Marcelo <oscarmarcelo@users.noreply.github.com>
2020-06-27 20:32:27 +0100

    Add "Name" Template

---
+++ src/common/templates/name.js
@@ -0,0 +1,33 @@
+export default {
+  name: 'Name',
+  icon: 'person',
+  data: {
+    tokens: [
+      {
+        id: '7e0fccb0-9906-46fe-bba5-47d2fcf97187',
+        type: 'data',
+        config: {
+          data: {
+            group: 'person',
+            item: 'first-name'
+          }
+        }
+      },
+      {
+        id: '99a06792-91be-47e6-b918-d09a168ac71f',
+        type: 'text',
+        text: ' '
+      },
+      {
+        id: 'bc6bbe49-3edc-4259-a99b-49dee0a35c3c',
+        type: 'data',
+        config: {
+          data: {
+            group: 'person',
+            item: 'last-name'
+          }
+        }
+      }
+    ]
+  }
+};
